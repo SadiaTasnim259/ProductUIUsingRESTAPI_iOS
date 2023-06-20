d265c2f720760fd0f070b06f3646d3534d6be41e
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-06-21 00:32:54 +0600

    Update Controller

---
+++ ProductUIUsingRESTAPI/Screens/UPDATE_Products/View/UpdateViewController.swift
@@ -0,0 +1,28 @@
+//
+//  UpdateViewController.swift
+//  ProductUIUsingRESTAPI
+//
+//  Created by Sadia on 21/6/23.
+//
+
+import UIKit
+
+class UpdateViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var quentityTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var imageTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func updateButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+
+}
